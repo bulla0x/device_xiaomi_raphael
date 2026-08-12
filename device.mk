f9de55587e3b74f74622f6d92d2d5621b358174e
bulla0x <jyotishmanbaruah67@gmail.com>
2026-08-13 00:27:31 +0530

    raphael: say hello to BCR

--- device.mk
+++ device.mk
@@ -32,6 +32,9 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
+# Call the BCR setup
+$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
+
 # Camera
 $(call inherit-product-if-exists, device/xiaomi/miuicamera/device.mk)
 

560de98bd74d60a918e8dc0d928c32eaaee54ab6
bulla0x <jyotishmanbaruah67@gmail.com>
2026-08-13 00:24:27 +0530

    raphael: adapt to ASCP

---
+++ raphael.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common ASCP stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+
+# Inherit from raphael device
+$(call inherit-product, device/xiaomi/raphael/device.mk)
+
+# Flags
+TARGET_DISABLE_EPPE = true
+ASCP_MAINTAINER = BULLA
+WITH_BCR := true
+WITH_REVANCED := true
+
+PRODUCT_NAME := raphael
+PRODUCT_DEVICE := raphael
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi 9T Pro
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="raphael-user 11 RKQ1.200826.002 V12.5.2.0.RFKMIXM release-keys" \
+    BuildFingerprint=Xiaomi/raphael/raphael:11/RKQ1.200826.002/V12.5.2.0.RFKMIXM:user/release-keys
